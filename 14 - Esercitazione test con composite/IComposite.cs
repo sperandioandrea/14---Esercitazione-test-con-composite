a7a5cc965dc314972d8e362da98a906d78e3eddd
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2024-01-11 10:54:35 +0100

    Creazioni Classi

---
+++ 14 - Esercitazione test con composite/IComposite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14___Esercitazione_test_con_composite
+{
+    public abstract class IComposite
+    {
+
+    }
+}
