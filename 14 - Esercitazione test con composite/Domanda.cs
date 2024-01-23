e96199257bfba9d352d1bcfef337f883ab9e0f1b
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2024-01-23 13:44:59 +0100

    classe domanda

--- 14 - Esercitazione test con composite/Domanda.cs
+++ 14 - Esercitazione test con composite/Domanda.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace _14___Esercitazione_test_con_composite
 {
-    public class Domanda
+    public class Domanda : IComposite
     {
+        private List<IComposite> risposte = new List<IComposite>();
 
+        public override void Aggiunta(IComposite componente)
+        {
+            risposte.Add(componente);
+        }
+
+        public override void Cancellazione(IComposite componente)
+        {
+            risposte.Remove(componente);
+        }
+
+        public override IComposite GetChild(int index)
+        {
+            return risposte[index];
+        }
+
+        public override int Punteggio()
+        {
+            int punteggioTotale = 0;
+            foreach (var risposta in risposte)
+            {
+                punteggioTotale += risposta.Punteggio();
+            }
+            return punteggioTotale;
+        }
     }
 }
